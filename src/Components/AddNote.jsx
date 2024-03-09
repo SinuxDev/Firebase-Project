65d0443d6a68736e4c28e205e72af9845118f3f7
Aung Ye Htet <aung.atg1738@gmail.com>
2024-03-09 17:03:31 +0630

    ErrorFixed

--- src/Components/AddNote.jsx
+++ src/Components/AddNote.jsx
@@ -1,49 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-const AddNote = ({getNotes}) => {
+const AddNote = ({ getNotes }) => {
+  // define state
+  const [note, setNote] = useState("");
 
-    // define state
-    const [note,setNote] = useState("");
-    
-    // add new note
-    const addNote = async (e) =>{
-        e.preventDefault();
+  // add new note
+  const addNote = async (e) => {
+    e.preventDefault();
 
-        if(note.trim().length == 0){
-            alert("Please enter an valid note")
-            return;
-        }
-        
-        try{
-            await fetch('https://wenoted-192e7-default-rtdb.firebaseio.com/notes.json',
-            {
-                method : "POST",
-                body : JSON.stringify(note),
-                headers : {
-                    "Content-Type" : "application/json",
-                }
-            });
+    if (note.trim().length === 0) {
+      alert("Please enter an valid note");
+      return;
+    }
 
-        setNote("");
-        getNotes();
-        }catch(err)
+    try {
+      await fetch(
+        "https://wenoted-192e7-default-rtdb.firebaseio.com/notes.json",
         {
-            alert("Something went wrong!!!");
+          method: "POST",
+          body: JSON.stringify(note),
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-        
-    };
+      );
+
+      setNote("");
+      getNotes();
+    } catch (err) {
+      alert("Something went wrong!!!");
+    }
+  };
 
   return (
     <section>
-        <form className='card' onSubmit={addNote} >
-            <input type="text" name="" id="" placeholder='add note' value={note} onChange=
-            {
-                (e) => setNote(e.target.value)
-            } />
-            <button className='submitBtn'>Add Note</button>
-        </form>
+      <form className="card" onSubmit={addNote}>
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="add note"
+          value={note}
+          onChange={(e) => setNote(e.target.value)}
+        />
+        <button className="submitBtn">Add Note</button>
+      </form>
     </section>
   );
 };
 
-export default AddNote
+export default AddNote;
